1883a1de6742c9eb46ee9a3c485ea5fbfa615eaf
wivi514 <148345636+wivi514@users.noreply.github.com>
2025-08-13 22:40:13 -0400

    UI change in hub + VR highlight UI

--- Assets/Script/Interaction/VRInteractionUI.cs
+++ Assets/Script/Interaction/VRInteractionUI.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class VRInteractionUI : MonoBehaviour
@@ -7,6 +8,8 @@ public class VRInteractionUI : MonoBehaviour
     public GameObject rightController;
     public LayerMask uiLayerMask;
     public float maxDistance = 10f;
+
+    private Button lastHoveredButton;
 
     private void Awake()
     {
@@ -25,6 +28,48 @@ public class VRInteractionUI : MonoBehaviour
             Vector3 start = rightController.transform.position;
             Vector3 end = start + rightController.transform.forward * maxDistance;
             Gizmos.DrawLine(start, end);
+        }
+    }
+
+    private void Update()
+    {
+        HandleUIHover();
+    }
+
+    private void HandleUIHover()
+    {
+        if (rightController == null)
+            return;
+
+        Transform controllerTransform = rightController.transform;
+        Ray ray = new Ray(controllerTransform.position, controllerTransform.forward);
+
+        if (Physics.Raycast(ray, out RaycastHit hit, maxDistance, uiLayerMask))
+        {
+            Button button = hit.collider.GetComponent<Button>();
+
+            if (button != null)
+            {
+                if (lastHoveredButton != button)
+                {
+                    // Quitter l'ancien bouton
+                    if (lastHoveredButton != null)
+                        ExecuteEvents.Execute(lastHoveredButton.gameObject, new PointerEventData(EventSystem.current), ExecuteEvents.pointerExitHandler);
+
+                    // Entrer dans le nouveau bouton
+                    ExecuteEvents.Execute(button.gameObject, new PointerEventData(EventSystem.current), ExecuteEvents.pointerEnterHandler);
+                    lastHoveredButton = button;
+                }
+            }
+        }
+        else
+        {
+            // Sortir du bouton si plus visé
+            if (lastHoveredButton != null)
+            {
+                ExecuteEvents.Execute(lastHoveredButton.gameObject, new PointerEventData(EventSystem.current), ExecuteEvents.pointerExitHandler);
+                lastHoveredButton = null;
+            }
         }
     }
 
